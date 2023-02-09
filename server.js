e69831828e4af2c44a423a98806d9971a17895fe
Timothy Mott <timothymott1@gmail.com>
2023-02-08 19:27:00 -0600

    update

--- server.js
+++ server.js
@@ -3,6 +3,64 @@ const app = express()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
 
+// Fake Data
+// const dinos = [
+//     {
+//     name: "Amargasaurus", 
+//     img: "", 
+//     diet: "Herbivore",
+//     tameable: true,
+//     rideable: true,
+//     temperament: "Aggressive"
+//     },
+
+//     { 
+//     name: "Kentrosaurus", 
+//     img: "", 
+//     diet: "Herbivore",
+//     tameable: true,
+//     rideable: false,
+//     temperament: "Short-Tempered"
+//     },
+
+//     {
+//     name: "Dilophosaur", 
+//     img: "", 
+//     diet: "Carnivore",
+//     tameable: true,
+//     rideable: false,
+//     temperament: "Territorial"
+//     },
+
+//     {
+//     name: "Spinosaurus", 
+//     img: "", 
+//     diet: "Carnivore",
+//     tameable: true,
+//     rideable: true,
+//     temperament: "Territorial"
+//     },
+
+//     {
+//     name: "Baryonyx", 
+//     img: "", 
+//     diet: "Piscivore",
+//     tameable: true,
+//     rideable: true,
+//     temperament: "Territorial"
+//     },
+
+//     {
+//     name: "Tyrannosaurus", 
+//     img: "", 
+//     diet: "Carnivore",
+//     tameable: true,
+//     rideable: true,
+//     temperament: "Aggresive"
+//     },
+
+// ]
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
