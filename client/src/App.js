c8071f51d2661f41c5afe41a523f64bba556b117
April Murillo <afreyde@wgu.edu>
2023-02-09 13:34:14 -0800

    finished all dino routes

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect} from 'react'
+// import axios from 'axios'
+import './App.css';
+
+export default function App(){
+  // const [] = useState([])
+  // useEffect(() => {
+  //   axios.get("/")
+  //   .then(res => console.log(res))
+  // }, [])
+  return(
+    <div>
+
+    </div>
+  )
+}
